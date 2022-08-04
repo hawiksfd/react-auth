82e349aefa5ff6190abc5fb94c8a4240a29398d2
hawik <hawiksyaef@gmail.com>
2022-08-04 15:43:56 +0700

    konfigurasi user list

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Userlist = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    const getUsers = async () => {
+        const response = await axios.get('http://localhost:5000/users');
+        setUsers(response.data);
+    }
+
+
+
     return (
         <div>
             <h1 className='title'>Users</h1>
             <h2 className='subtitle'>List of Users</h2>
+            <Link to="/users/add" className='button is-primary mb-2'>Add new</Link>
             <table className='table is-stripped is-fullwidth'>
                 <thead>
                     <tr>
@@ -16,13 +32,18 @@ const Userlist = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
+                    {users.map((users, index) => (
+
+                        <tr key={users.uuid}>
+                            <td>{index + 1}</td>
+                            <td>{users.name}</td>
+                            <td>{users.email}</td>
+                            <td>{users.role}</td>
+                            <td>
+
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
 
             </table>
