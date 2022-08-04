b92201ceb5f56bd175822ee94c7f62b41b27c61e
hawik <hawiksyaef@gmail.com>
2022-08-04 14:34:43 +0700

    konfigurasi protected all page

--- frontend/src/pages/AddProduct.jsx
+++ frontend/src/pages/AddProduct.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import FormAddProduct from '../components/FormAddProduct'
+import { getMe } from '../features/authSlice';
 import Layout from './Layout'
 
 const AddProduct = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { isError } = useSelector((state => state.auth));
+
+    useEffect(() => {
+        dispatch(getMe());
+    }, [dispatch]);
+
+    useEffect(() => {
+        if (isError) {
+            navigate('/');
+        }
+    }, [isError, navigate]);
+
     return (
         <Layout>
             <FormAddProduct />
